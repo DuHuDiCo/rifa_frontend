c514ceabf164bfc90816c7ce09de55a3d58c8a2b
Duvan <dudicodiaz@gmail.com>
2023-11-22 16:44:33 -0500

    c

--- src/app/Utils/helper.ts
+++ src/app/Utils/helper.ts
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 //let baseUrl = "https://rifagmj-production.up.railway.app/api/v1"
 let baseUrl = "http://rifagmj-oriente.duckdns.org/api/v1"
 export default baseUrl;
-=======
-let baseUrl = "http://programs-backend.duckdns.org"
-export default baseUrl;
->>>>>>> 913857e7e24253689c4cb730da234c56c60a4ca9
