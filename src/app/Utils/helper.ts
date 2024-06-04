bb1c174f3d6f729ed7ac375aa25ea58043015853
4rtemis-25 <121249378+4rtemis-25@users.noreply.github.com>
2024-06-04 15:22:33 -0500

    c

--- src/app/Utils/helper.ts
+++ src/app/Utils/helper.ts
@@ -1,3 +1,3 @@
 //let baseUrl = "https://rifagmj-production.up.railway.app/api/v1"
-let baseUrl = "http://programs-backend.duckdns.org/api/v1"
+let baseUrl = "http://rifa-gmj.duckdns.org/api/v1"
 export default baseUrl;
